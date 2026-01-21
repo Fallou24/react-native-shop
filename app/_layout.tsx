5c0ecdca4338f9f9c08baab86e79336eb1c0fc17
Fallou24 <falloundiaye0221@gmail.com>
2026-01-21 23:17:14 +0000

    Create Pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(shop)" options={{ headerShown: false }} />;
+      <Stack.Screen name="categories" options={{ headerShown: false }} />;
+      <Stack.Screen name="products" options={{ headerShown: false }} />;
+      <Stack.Screen name="cart" options={{ headerShown: false }} />;
+      <Stack.Screen name="auth" options={{ headerShown: false }} />;
+    </Stack>
+  );
 }
