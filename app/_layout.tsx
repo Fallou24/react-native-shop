d0fce2c8ae6ee8af663f9b8c358b8c305bfd809a
Fallou24 <falloundiaye0221@gmail.com>
2026-01-22 13:50:33 +0000

    Cart page design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
       <Stack.Screen name="(shop)" options={{ headerShown: false }} />;
       <Stack.Screen name="categories" options={{ headerShown: false }} />;
       <Stack.Screen name="product" options={{ headerShown: false }} />;
-      <Stack.Screen name="cart" options={{ headerShown: false }} />;
+      <Stack.Screen name="cart" options={{ headerShown: true }} />;
       <Stack.Screen name="auth" options={{ headerShown: false }} />;
     </Stack>
   );
