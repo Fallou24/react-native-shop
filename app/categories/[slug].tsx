001ff07acb31004636e0a9447d4bf84e07cf40a6
Fallou24 <falloundiaye0221@gmail.com>
2026-01-22 11:35:11 +0000

    Category Page

--- app/categories/[slug].tsx
+++ app/categories/[slug].tsx
@@ -1,15 +1,75 @@
-import { Text, View } from "react-native";
+import { CATEGORIES } from "@/assets/categories";
+import { ProductListItem } from "@/components/product-list-item";
+import { Image } from "expo-image";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const { slug } = useLocalSearchParams();
+  const category = CATEGORIES.find((item) => item.slug === slug);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Categories</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: category?.name }} />
+      <Image
+        source={{ uri: category?.imageUrl }}
+        style={styles.categoryImage}
+      />
+      <Text style={styles.categoryName}>{category?.name}</Text>
+      <FlatList
+        data={category?.products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        columnWrapperStyle={styles.productRow}
+        contentContainerStyle={styles.productsList}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  categoryImage: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  categoryName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  productsList: {
+    flexGrow: 1,
+  },
+  productRow: {
+    justifyContent: "space-between",
+  },
+  productContainer: {
+    flex: 1,
+    margin: 8,
+  },
+  productImage: {
+    width: "100%",
+    height: 150,
+    resizeMode: "cover",
+    borderRadius: 8,
+  },
+  productTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: 8,
+  },
+  productPrice: {
+    fontSize: 14,
+    color: "#888",
+    marginTop: 4,
+  },
+});
--- app/categories/_layout.tsx
+++ app/categories/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="[slug]" options={{ headerShown: false }} />;
+      <Stack.Screen name="[slug]" options={{ headerShown: true }} />;
     </Stack>
   );
 }
